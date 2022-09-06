70803da10f734da859f7688d41e7698dd4c272b0
Aleyna İlbeyi <aleynailbeyi@192.168.1.54>
2022-09-06 14:42:37 +0300

    solved mini max problem

---
+++ mini-max.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    arr=arr.sort()
+    let min = 0, max = 0
+    arr.forEach((num, index) => {
+        if(index == 0) min += num
+        else if(index == arr.length-1) max += num
+        else {
+            min += num
+            max += num
+        }
+    })
+    console.log(min, max)
+}
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
