eb1726fdbbecac2a4d461596a2c0861d44585c82
Aleyna İlbeyi <aleynailbeyi@192.168.1.54>
2022-09-06 14:19:45 +0300

    solved plus minus problem

---
+++ plusMinus.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    let numbers = [0, 0, 0]
+    arr.forEach(num => {
+        if(num > 0) numbers[0] += 1
+        else if(num < 0) numbers[1] += 1
+        else numbers[2] +=1
+    })    
+    numbers.forEach(num =>
+    console.log((num/arr.length).toFixed(6)))
+    return
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
